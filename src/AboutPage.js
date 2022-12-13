a5c5017a60a2d6e2718c8e5dd9ebc6702b3fad72
Anna Bush <annabushsoftware@gmail.com>
2022-12-13 17:49:22 -0500

    adjust stylesheets

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,6 +1,6 @@
 export default function AboutPage() {
     return (
-        <div>
+        <div className="about-page">
             <h1>About</h1>
             <h2>This tutorial shows how to migrate from Create React App to Next.js</h2>
         </div>
